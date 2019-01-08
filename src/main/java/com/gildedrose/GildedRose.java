097209a32f425cee6fea41ca4a720f2c6a6ce3ff
MoulinDigital <MoulinDigital@UC20353.ladrome.local>
2019-01-08 11:50:24 +0100

    adding latest logger

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -41,12 +41,14 @@ public class GildedRose {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
+                                logger.info("{} quality for backstage with sellIn < 11", items[i].quality);
                             }
                         }
 
                         if (items[i].sellIn < 6) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
+                                logger.info("{} quality for backstage with sellIn < 6", items[i].quality);
                             }
                         }
                     }
@@ -59,22 +61,29 @@ public class GildedRose {
 
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
+                logger.info("{} quality for all exept sulfuras", items[i].sellIn);
             }
 
             if (items[i].sellIn < 0) {
+                logger.info("sellIn is {}", items[i].sellIn);
+
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1;
+                                logger.info("{} quality for all exept brie, Backstage and sulfuras when quality{} ", items[i].quality, items[i].sellIn);
+
                             }
                         }
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
+                        logger.info("{} quality for Backstage and sulfuras when quality{} ", items[i].quality);
                     }
                 } else {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
+                        logger.info("{} quality for brie when quality {} ", items[i].quality);
                     }
                 }
             }
