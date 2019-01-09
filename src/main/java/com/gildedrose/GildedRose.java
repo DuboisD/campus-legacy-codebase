6020cdbf06f0a562b2e9d53631b11b56c869f8b3
MoulinDigital <MoulinDigital@UC20353.ladrome.local>
2019-01-09 10:03:36 +0100

    my own version

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,85 +9,65 @@ public class GildedRose {
         this.items = items;
     }
 
+    Logger logger = LoggerFactory.getLogger(GildedRose.class);
+
     public void updateQuality() {
 
-        Logger logger = LoggerFactory.getLogger(GildedRose.class);
-
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 
-                logger.info(items[i].name);
-
-                if (items[i].quality > 0) {
-
-                    logger.info("item[i].quality is above 0");
-
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        if (!items[i].name.equals("Conjured Mana Cake")) {
-                            items[i].quality = items[i].quality - 1;
-                            logger.info("{} quality for all exept sulfuras", items[i].quality);
-
-                        } else {
-                            items[i].quality = items[i].quality - 2;
-                            logger.info("{} quality for sulfuras", items[i].quality);
-                        }
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                                logger.info("{} quality for backstage with sellIn < 11", items[i].quality);
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                                logger.info("{} quality for backstage with sellIn < 6", items[i].quality);
-                            }
-                        }
-                    }
-                } else {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        logger.debug("{} is above 50", items[i].quality);
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!items[i].name.startsWith("Sulfuras")) {
                 items[i].sellIn = items[i].sellIn - 1;
-                logger.info("{} quality for all exept sulfuras", items[i].sellIn);
             }
 
-            if (items[i].sellIn < 0) {
-                logger.info("sellIn is {}", items[i].sellIn);
-
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (items[i].sellIn >= 0) {
+                if (items[i].quality >= 0) {
+                    if (!items[i].name.equals("Aged Brie") && !items[i].name.startsWith("Backstage passes")) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            items[i].quality = items[i].quality - 1;
+                            if (items[i].name.startsWith("Conjured")) {
                                 items[i].quality = items[i].quality - 1;
-                                logger.info("{} quality for all exept brie, Backstage and sulfuras when quality{} ", items[i].quality, items[i].sellIn);
-
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                        logger.info("{} quality for Backstage and sulfuras when quality{} ", items[i].quality);
+                        if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1;
+                            if (items[i].name.startsWith("Backstage passes")) {
+                                if (items[i].sellIn < 11) {
+                                    if (items[i].quality < 50) {
+                                        items[i].quality = items[i].quality + 1;
+                                    }
+                                }
+                                if (items[i].sellIn < 6) {
+                                    if (items[i].quality < 50) {
+                                        items[i].quality = items[i].quality + 1;
+                                    }
+                                }
+                            }
+                        }
                     }
                 } else {
-                    if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality - items[i].quality;
+                }
+
+
+            } else {
+                if (items[i].quality > 0) {
+                    if (!items[i].name.startsWith("Sulfuras")
+                            && !items[i].name.equals("Aged Brie")
+                            && !items[i].name.startsWith("Backstage passes")) {
+                            items[i].quality = items[i].quality - 1;
+                    } else if (items[i].name.equals("Aged Brie")) {
                         items[i].quality = items[i].quality + 1;
-                        logger.info("{} quality for brie when quality {} ", items[i].quality);
+                    } else {
+                        items[i].quality = items[i].quality - items[i].quality;
                     }
                 }
             }
+
+            logger.info("name is {}, sellIn is {} and quality is {}", items[i].name, items[i].sellIn, items[i].quality);
+
         }
+
     }
 
     public Item[] getItems() {
